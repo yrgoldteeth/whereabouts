5716b9080dc57a525ca3b3b85638b1842e761128
Nicholas Fine <nicholas.fine@gmail.com>
2011-06-14 16:58:01 -0500

    remove static validations from address model; make validations defined on load; syntax cleanup

--- lib/generators/address/templates/address.rb
+++ lib/generators/address/templates/address.rb
@@ -2,7 +2,6 @@ class Address < ActiveRecord::Base
   belongs_to :addressable, :polymorphic => true
   ZIP_REGEX   = /^\d{5}([\-]\d{4})?$/
 
-  validates_presence_of :city, :state, :zip
   validates_format_of :zip, :with => ZIP_REGEX, :message => 'is invalid'
 
   def geocode_address
--- lib/whereabouts_methods.rb
+++ lib/whereabouts_methods.rb
@@ -55,22 +55,49 @@ module Yrgoldteeth
         # type of Address.  Defaults to the parent class.
         def has_whereabouts klass=:address, options={}
           unless klass == :address
-            create_address_class(klass.to_s.classify)
+            create_address_class(klass.to_s.camelize)
           end
           
           # Set the has_one relationship and accepts_nested_attributes_for.  
           has_one klass, :as => :addressable, :dependent => :destroy
           accepts_nested_attributes_for klass
 
+          # Check for fields to validate
+          # options[:validate] should be an array of symbols that match
+          # attributes to validates_presence_of on the Address.
+          # Define Class.validate_#{klass)_whereabouts? in the event that 
+          # multiple models share the same Address class but validations
+          # are not required for all instances.
+          _singleton = "validate_#{klass.to_s}_whereabouts?".to_sym
+          if options[:validate]
+            set_validators(klass, options[:validate])
+            define_singleton_method _singleton do true end
+          else
+            define_singleton_method _singleton do false end
+          end
+
           # Check for geocode in options
           # and confirm geocoder is defined
-          if options[:geocode] && defined?(Geocoder)
+          if options[:geocode] && options[:geocode] == true && defined?(Geocoder)
             set_geocoding(klass)
           end
         end
 
+        # Sets validates_presence_of fields for the Address based on the 
+        # validate_#{klass}_whereabouts? singleton method created on the Address'
+        # addressable_type class.
+        def set_validators klass, fields=[]
+          _singleton = "validate_#{klass.to_s}_whereabouts?".to_sym
+          klass.to_s.camelize.constantize.class_eval do
+            fields.each do |f|
+              validates_presence_of f, :if => lambda {|a| a.addressable_type.constantize.send _singleton}
+            end
+          end
+        end
+
+        # If defined, geocode the address.
         def set_geocoding klass
-          klass.to_s.titleize.constantize.class_eval do
+          klass.to_s.camelize.constantize.class_eval do
             geocoded_by :geocode_address
             after_validation :geocode, :if => lambda {|o| o.new_record? || o.changed?}
           end
