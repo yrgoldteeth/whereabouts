1c88d5d5a812092e63c681f7ba3b1234e932aea4
Nicholas Fine <nicholas.fine@gmail.com>
2012-08-03 14:37:18 -0500

    remove unused railtie

--- lib/whereabouts.rb
+++ lib/whereabouts.rb
@@ -1,11 +1,4 @@
 # Whereabouts
 require 'whereabouts_methods'
 module Whereabouts
-  module Rails
-    class Railtie < ::Rails::Railtie
-      unless File.exist?('app/models/address.rb')
-        puts 'NOTICE: Please run rails generate address for whereabouts gem'
-      end
-    end
-  end
 end
